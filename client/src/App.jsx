eb7287155f82e151309971089c3509f053180011
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-09-11 16:18:59 +0530

    routes complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React from 'react';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
+import About from './pages/About';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import Dashboard from './pages/Dashboard';
 
 function App() {
   return (
-    <div className='text-4xl font-bold'>App</div>
+    <BrowserRouter>
+      
+      <Routes>
+
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path='/sign-up' element={<SignUp />} />
+        <Route path='/dashboard' element={<Dashboard />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
