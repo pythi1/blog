108561037e3a23f0f2841de6f728c6bab73e8e7c
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-09-13 11:58:34 +0530

    done with the header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,10 +6,13 @@ import About from './pages/About';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Dashboard from './pages/Dashboard';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <BrowserRouter>
+    <Header />
       
       <Routes>
 
@@ -21,6 +24,8 @@ function App() {
         <Route path='/dashboard' element={<Dashboard />} />
 
       </Routes>
+
+      <Footer />
 
     </BrowserRouter>
   )
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,69 @@
+import React from 'react';
+import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
+import { Link, useLocation } from 'react-router-dom';
+import { AiOutlineSearch } from 'react-icons/ai';
+import { FaMoon } from 'react-icons/fa';
+
+function Header() {
+
+    const path = useLocation().pathname;
+    return (
+        <div>
+            <Navbar className='border-b-2 ' >
+                <Link to={"/home"} className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white' >
+                    <span className='border px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'  > Maverick Thought </span> Community
+                </Link>
+
+                <form >
+                    <TextInput
+                        placeholder='search...'
+                        type='text'
+                        rightIcon={AiOutlineSearch}
+                        className='hidden lg:inline'
+                    />
+                    
+                </form>
+                <Button className='w-12 h-10 lg:hidden' pill color='gray' >
+                    <AiOutlineSearch />
+                </Button>
+
+                <div className='flex gap-2 md:order-2' >
+                    <Button className='w-12 h-10 hidden sm:inline' color='gray' pill >
+                        <FaMoon />
+                    </Button>
+                    <Link to="/sign-in">
+                    <Button className='' gradientDuoTone='purpleToBlue' >
+                        Sign In
+                    </Button>
+                    </Link>
+                    <NavbarToggle />
+
+                </div>
+
+                <Navbar.Collapse >
+                        <Navbar.Link active={path === "/home"} >
+                            <Link to='/home' className='font-bold' >
+                                Home
+                            </Link>
+                        </Navbar.Link>
+
+                        <Navbar.Link active={path === "/about"} >
+                            <Link to='/about' className='font-bold' >
+                                About
+                            </Link>
+                        </Navbar.Link>
+
+                        <Navbar.Link active={path === "/projects"} >
+                            <Link to='/projects' className='font-bold' >
+                                Projects
+                            </Link>
+                        </Navbar.Link>
+
+                    </Navbar.Collapse>
+
+            </Navbar>
+        </div>
+    )
+}
+
+export default Header
