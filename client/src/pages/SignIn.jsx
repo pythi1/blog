dd721b36ba208c0899a37396e627ea14cf7bc858
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-09-16 16:55:58 +0530

    complete sign in functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,120 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react';
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom';
 
 function SignIn() {
+
+  const [formdata, setfromdata] = useState({});
+  const [errormessage, setErrormessage] = useState(null);
+  const [loading, setloading] = useState(false);
+
+  const navigate = useNavigate();
+
+  const handlechange = (e) => {
+    
+    setfromdata({ ...formdata, [e.target.id]: e.target.value.trim() });
+  }
+
+  console.log(formdata);
+
+  // **********************************************************************************
+                  // form submit function //
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!formdata.email || !formdata.password ){
+      return setErrormessage('please fill out all the fields.')
+    }
+    try {
+      setloading(true);
+      setErrormessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(formdata),
+      })
+      const data = await res.json();
+      if(data.success === false){
+        return setErrormessage(data.message);
+      }
+      setloading(false);
+      if(res.ok){
+        navigate('/home');
+      }
+    } catch (error) {
+      setErrormessage(error.message);
+      setloading(false);
+    }
+  };
+  
+  // **********************************************************************************
+
   return (
-    <div>SignIn</div>
+    <div className='min-h-screen mt-20'>
+      <div className='flex p-3 max-w-3xl mx-auto gap-2 flex-col md:flex-row md:items-center'>
+
+        <div className='flex-1 '>
+          {/* leftside */}
+          <Link to={"/home"} className='font-bold dark:text-white lg:text-4xl text-2xl' >
+            <span className='border px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'  > Maverick Thought </span> Community
+          </Link>
+
+          <p className='text-sm mt-5 '>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste maiores sequi porro quo omnis, eos ullam mollitia saepe iusto animi, inventore quas rerum ab nisi sint voluptates quos. Voluptatum, sit!
+          </p>
+        </div>
+
+        <div className="lg:w-[1px] lg:h-52 lg:mt-5 lg:bg-gray-400 lg:mx-4 "></div>
+
+        <div className='flex-1'>
+          {/* rightside */}
+          <form className='flex flex-col gap-4 ' onSubmit={handleSubmit} >
+          
+            <div>
+              <Label value='Email' />
+              <TextInput
+                type='email'
+                placeholder='example@gmail.com'
+                id='email'
+                onChange={handlechange}
+              />
+            </div>
+
+            <div>
+              <Label value='Password' />
+              <TextInput
+                type='password'
+                placeholder='*******'
+                id='password'
+                onChange={handlechange}
+              />
+            </div>
+
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+              {
+                loading ? <><Spinner size='sm' /> <span className='pl-3' >loading...</span></> : <span>Sign In</span>
+              }
+            </Button>
+
+          </form>
+
+          <div className='flex gap-2 text-sm mt-5'>
+            <span>Don't Have an account ? </span>
+            <Link to='/sign-up' className='text-blue-500' >signup</Link>
+          </div>
+
+          {
+            errormessage && (
+              <Alert className='mt-5' color='failure' >
+                {errormessage}
+              </Alert>
+            )
+          }
+
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default SignIn
+export default SignIn;
