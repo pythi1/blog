5b17deccfe7538dce273586f3cdc50851d07b144
Pramod <134283019+pramodjangra@users.noreply.github.com>
2024-10-09 16:10:12 +0530

    complete darkmode functionality

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
+import { Avatar, Button, Dropdown, DropdownHeader, Navbar, NavbarToggle, TextInput } from 'flowbite-react';
 import { Link, useLocation } from 'react-router-dom';
 import { AiOutlineSearch } from 'react-icons/ai';
-import { FaMoon } from 'react-icons/fa';
+import { FaMoon, FaSun } from 'react-icons/fa';
+import { useSelector, useDispatch } from 'react-redux';
+import { toggleTheme } from '../Redux/theme/themeSlice';
 
 function Header() {
 
     const path = useLocation().pathname;
+    const dispatch = useDispatch();
+    const { theme } = useSelector(state => state.theme);
+    const { currentUser } = useSelector(state => state.user);
+
     return (
         <div>
             <Navbar className='border-b-2 ' >
@@ -21,45 +27,85 @@ function Header() {
                         rightIcon={AiOutlineSearch}
                         className='hidden lg:inline'
                     />
-                    
+
                 </form>
                 <Button className='w-12 h-10 lg:hidden' pill color='gray' >
                     <AiOutlineSearch />
                 </Button>
 
                 <div className='flex gap-2 md:order-2' >
-                    <Button className='w-12 h-10 hidden sm:inline' color='gray' pill >
-                        <FaMoon />
+                    <Button
+                        className='w-12 h-10 hidden sm:inline'
+                        color='gray'
+                        pill
+                        onClick={() => dispatch(toggleTheme())}
+                    >
+                        {/* <FaMoon /> */}
+                        {theme === "light" ? <FaMoon /> : <FaSun /> }
+
                     </Button>
-                    <Link to="/sign-in">
-                    <Button outline gradientDuoTone='purpleToBlue' >
-                        Sign In
-                    </Button>
-                    </Link>
+                    {
+                        currentUser ?
+                            (
+                                <Dropdown
+                                    className='bg-gradient-to-t from-red-300 via-pink-400 to-slate-100'
+                                    arrowIcon={false}
+                                    inline
+                                    label={
+                                        <Avatar
+                                            alt='user'
+                                            img={currentUser.profilePicture}
+                                        />
+                                    }
+                                >
+                                    <Dropdown.Header>
+                                        <span className='block text-sm' >@{currentUser.username}</span>
+                                        <span className='block text-sm font-medium truncate' >{currentUser.email}</span>
+                                    </Dropdown.Header>
+                                    <Link to={'/dashboard?tab=profile'} >
+                                        <Dropdown.Item>Profile</Dropdown.Item>
+                                    </Link>
+                                    <Dropdown.Divider />
+
+                                    <Dropdown.Item>SignOut</Dropdown.Item>
+
+
+                                </Dropdown>
+                            )
+                            :
+                            (
+                                <Link to="/sign-in">
+                                    <Button outline gradientDuoTone='purpleToBlue' >
+                                        Sign In
+                                    </Button>
+                                </Link>
+                            )
+                    }
+
                     <NavbarToggle />
 
                 </div>
 
                 <Navbar.Collapse >
-                        <Navbar.Link active={path === "/home"} >
-                            {/* <Link to='/home' className='font-bold' > */}
-                                Home
-                            {/* </Link> */}
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/home"} >
+                        <Link to='/home' className='font-bold' >
+                            Home
+                        </Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/about"} >
-                            <Link to='/about' className='font-bold' >
-                                About
-                            </Link>
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/about"} >
+                        <Link to='/about' className='font-bold' >
+                            About
+                        </Link>
+                    </Navbar.Link>
 
-                        <Navbar.Link active={path === "/projects"} >
-                            <Link to='/projects' className='font-bold' >
-                                Projects
-                            </Link>
-                        </Navbar.Link>
+                    <Navbar.Link active={path === "/projects"} >
+                        <Link to='/projects' className='font-bold' >
+                            Projects
+                        </Link>
+                    </Navbar.Link>
 
-                    </Navbar.Collapse>
+                </Navbar.Collapse>
 
             </Navbar>
         </div>
